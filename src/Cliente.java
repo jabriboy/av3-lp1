2097b6d91b3f3e95b8c70b5c6bad36a114675579
Gabriel Rocha <jabrirocha@hotmail.com>
2022-12-03 18:36:51 -0300

    first commit

---
+++ src/Cliente.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Cliente {
+    String nome;
+    int id;
+    ArrayList<Pet> pets = new ArrayList<>();
+
+    public Cliente(){}
+
+    public Cliente(String nome){
+        this.nome = nome;
+    }
+
+    public void addPets(String nome, String raca, int idade){
+        pets.add(new Pet(nome, raca, idade));
+
+    }
+
+    public void removePet(String nome, String raca){
+        pets.forEach(n -> { if(n.nome == nome); });
+    }
+}
---
+++ src/Pet.java
@@ -0,0 +1,14 @@
+public class Pet {
+    String nome;
+    String raca;
+    int idade;
+
+    public Pet(){}
+
+    public Pet(String nome, String raca, int idade){
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+    }
+    
+}
